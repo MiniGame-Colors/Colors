72f7a141afccbf03df043bafff761bf7c9d76e59
jskyzero <jskyzero@outlook.com>
2017-12-11 04:10:24 +0800

    finished altar

---
+++ Assets/Scripts/Altar/AutomaticFracture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticFracture : MonoBehaviour
+{
+
+    public GameObject chain;
+    public float time;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Fracture());
+        }
+    }
+
+    private IEnumerator Fracture()
+    {
+        yield return new WaitForSeconds(time);
+        chain.GetComponent<HingeJoint2D>().enabled = false;
+    }
+}
