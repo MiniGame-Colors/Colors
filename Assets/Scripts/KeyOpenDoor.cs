46db4ba8e8bfe822f17ab213726d3a176fc2689a
jskyzero <jskyzero@outlook.com>
2017-12-11 03:01:03 +0800

    Altar first room finished

---
+++ Assets/Scripts/KeyOpenDoor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyOpenDoor : MonoBehaviour {
+
+    public GameObject wall; 
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // open the door
+            // may be there is a better wall than SetActive
+            wall.SetActive(false);
+            // delete key
+            gameObject.SetActive(false);
+        }
+    }
+}
