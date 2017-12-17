242c87e2f6ada7c72bfb95e72995dede27ffc996
jskyzero <jskyzero@outlook.com>
2017-12-17 13:32:01 +0800

    improve message dispaly details: 1. change background. 2. change text dispaly size (both font size and windows size). 3. change text dispaly content. 4. change bookshelf text display interface.

--- Assets/Scripts/hall/Bookshelf.cs
+++ Assets/Scripts/hall/Bookshelf.cs
@@ -4,20 +4,27 @@ using UnityEngine;
 
 public class Bookshelf : MonoBehaviour {
 
-    public bool active;
-
+    public bool activePower;
+    private string activePowerMessage = @"窃取颜色的人，太过愚蠢
+我不知昏迷多时，世界亦黯然受苦
+请把颜色还给我们。
+神啊，你心已经向我，不然这火焰，如何熊熊燃起。";
+    private string otherMessage = @"《论颜色》
+物质产生的不同颜色的物理特性，被人们称为颜色。
+混沌中的物质需要通过颜色，成为独一无二的存在。
+其中，红、黄、蓝被称为三原色，它们可以混合出世界上所有的颜色。";
 
     void OnTriggerEnter2D(Collider2D other) {
 
         if (other.CompareTag("Player")) {
-            if (active) {
+            if (activePower) {
                 DataTransformer.Awakening = true;
 
-                PromptManager.Instance.PromptShow("一场邪恶的祭祀仪式，必须要献上颜色");
+                PromptManager.Instance.PromptShow(activePowerMessage);
 
             }else {
 
-                PromptManager.Instance.PromptShow("论颜色的重要性");
+                PromptManager.Instance.PromptShow(otherMessage);
             }
         }
 
